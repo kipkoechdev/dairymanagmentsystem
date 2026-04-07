acb58f3dd432288f5436623b61ad00a2337775bf
Kipkoechdev <kipkoechb704@gmail.com>
2026-04-07 21:02:54 +0300

    trying to fix vercel deployment issues

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dairy_project.settings')
 
 def main():
     """Run administrative tasks."""
