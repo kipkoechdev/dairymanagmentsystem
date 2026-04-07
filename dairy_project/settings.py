28232fd313eacd1854c8b582f51f6ce0574abcbd
Kipkoechdev <kipkoechb704@gmail.com>
2026-04-07 21:29:24 +0300

    fix settings and static

--- dairy_project/settings.py
+++ dairy_project/settings.py
@@ -86,8 +86,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
